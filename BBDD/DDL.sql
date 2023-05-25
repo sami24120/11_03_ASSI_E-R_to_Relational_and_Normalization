9cb07c62cd9152683a5077a3b7753ea0ea0393fe
sami24120 <116269453+sami24120@users.noreply.github.com>
2023-05-25 18:55:52 +0200

    Update DDL.sql

--- BBDD/DDL.sql
+++ BBDD/DDL.sql
@@ -1,3 +1,22 @@
+CREATE TABLE Personas (
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  phone_number VARCHAR(255)
+);
+
+CREATE TABLE Entradas (
+  code INT PRIMARY KEY,
+  codePartidos INT,
+  FOREIGN KEY (codePartidos) REFERENCES Partidos(code)
+);
+
+CREATE TABLE PERSONA_BUY_ENTRADAS (
+  codeEntradas INT,
+  idPersonas INT,
+  precio DECIMAL(10, 2),
+  FOREIGN KEY (codeEntradas) REFERENCES Entradas(code),
+  FOREIGN KEY (idPersonas) REFERENCES Personas(id)
+);
 
 CREATE TABLE Partidos (
   code INT PRIMARY KEY,
@@ -5,47 +24,47 @@ CREATE TABLE Partidos (
   game_date DATE
 );
 
+CREATE TABLE Goles (
+  goles_por_jugador INT,
+  minute_gol INT,
+  codePartidos INT,
+  FOREIGN KEY (codePartidos) REFERENCES Partidos(code)
+);
 
 CREATE TABLE Players (
   id INT PRIMARY KEY,
-  posicion_campo VARCHAR(255),
+  posicion_Campo VARCHAR(255),
   nombre VARCHAR(255),
-  birth_date DATE
+  birth_Date DATE,
+  idTeams INT,
+  FOREIGN KEY (idTeams) REFERENCES Teams(id)
 );
 
-
 CREATE TABLE Portero (
-  id INT PRIMARY KEY,
-  paradas_por_partido INT,
+  idPlayers INT,
   gol_por_partido INT,
-  FOREIGN KEY (id) REFERENCES Players(id)
+  Paradas_por_partido INT,
+  FOREIGN KEY (idPlayers) REFERENCES Players(id)
+);
+
+CREATE TABLE Jugador_de_Campo (
+  idPlayers INT,
+  Gol_por_partido INT,
+  Minutos_jugados INT,
+  FOREIGN KEY (idPlayers) REFERENCES Players(id)
 );
 
 CREATE TABLE Teams (
   id INT PRIMARY KEY,
+  name_team VARCHAR(255),
   name_stadium VARCHAR(255),
-  ciudad_fundada VARCHAR(255),
-  name_team VARCHAR(255)
+  ciudadFundada DATE
 );
-
-
-CREATE TABLE Stadium (
-  id INT PRIMARY KEY,
-  name_stadium VARCHAR(255)
-);
-
-
-CREATE TABLE Seat (
-  id INT PRIMARY KEY,
-  seat_number INT
-);
-
 
 CREATE TABLE Seasons (
-  code INT PRIMARY KEY,
+  code INT,
   date DATE
 );
-
 
 CREATE TABLE Presidente (
   id INT PRIMARY KEY,
@@ -56,16 +75,22 @@ CREATE TABLE Presidente (
   equipo_lidera VARCHAR(255)
 );
 
-
-CREATE TABLE Goles (
+CREATE TABLE Stadium (
   id INT PRIMARY KEY,
-  goles_por_jugador INT,
-  minute_gol INT
+  name_Stadium VARCHAR(255)
 );
 
-
-CREATE TABLE JugadorDeCampo (
+CREATE TABLE Seat (
   id INT PRIMARY KEY,
-  gol_por_partido INT,
-  minutos_jugados INT
+  n_seat INT,
+  idStadium INT,
+  FOREIGN KEY (idStadium) REFERENCES Stadium(id)
 );
+
+CREATE TABLE Players_Idol_Players (
+  idPlayers INT,
+  idIdol INT,
+  FOREIGN KEY (idPlayers) REFERENCES Players(id),
+  FOREIGN KEY (idIdol) REFERENCES Players(id)
+);
+
