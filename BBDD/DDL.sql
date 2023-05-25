81ccbcb7a0dd7131127f2811c99e8daa82f0d693
kevin-coaquira <91737963+kevin-coaquira@users.noreply.github.com>
2023-05-25 23:14:34 +0200

    Update DDL.sql

--- BBDD/DDL.sql
+++ BBDD/DDL.sql
@@ -21,7 +21,29 @@ CREATE TABLE PERSONA_BUY_ENTRADAS (
 CREATE TABLE Partidos (
   code INT PRIMARY KEY,
   name_team VARCHAR(255),
-  game_date DATE
+  game_date DATE,
+  idArbitraje INT,
+  FOREIGN KEY (idPartidos) REFERENCES Arbitraje(id)
+);
+
+CREATE TABLE Partidos_Local_Teams(
+  codePartido INT,
+  idTeams INT,
+  FOREIGN KEY (codePartido) REFERENCES Partido(code),
+  FOREIGN KEY (idTeams) REFERENCES Teams(id)
+);
+
+CREATE TABLE Partidos_Visitante_Teams(
+  codePartido INT,
+  idTeams INT,
+  FOREIGN KEY (codePartido) REFERENCES Partido(code),
+  FOREIGN KEY(idTeams) REFERENCES Teams(id)
+);
+
+CREATE TABLE Arbitraje(
+  id INT, 
+  name VARCHAR(30),
+  faltas_por_partidos INT
 );
 
 CREATE TABLE Goles (
@@ -75,9 +97,25 @@ CREATE TABLE Presidente (
   equipo_lidera VARCHAR(255)
 );
 
+CREATE TABLE Teams_Has_Seasons_Has_Presidente(
+  idTeams INT,
+  codeSeasons INT,
+  idPresidente INT,
+  FOREIGN KEY (idTeams) REFERENCES Teams(id),
+  FOREIGN KEY (codeSeasons) REFERENCES Seasons(code),
+  FOREIGN KEY (idPresidente) REFERENCES Presidente(id)
+);
+
+CREATE TABLE Team_Has_Stadium(
+  idStadium INT,
+  idTeams INT,
+  FOREIGN KEY (idStadium) REFERENCES Stadium(id),
+  FOREIGN KEY (idTeams) REFERENCES Teams(id)
+);
+
 CREATE TABLE Stadium (
   id INT PRIMARY KEY,
-  name_Stadium VARCHAR(255)
+  name_stadium VARCHAR(255)
 );
 
 CREATE TABLE Seat (
